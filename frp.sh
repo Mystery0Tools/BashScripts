23d4a64215f6b41cf9fb238bd43aa329d6f3abcf
Mystery00 <mystery0dyl520@gmail.com>
2019-08-12 14:06:00 +0800

    fix: 脚本授权错误

--- frp.sh
+++ frp.sh
@@ -4,7 +4,7 @@
 # 该脚本参考了Toyo的脚本源码
 #
 
-sh_ver="1.0.0"
+sh_ver="1.0.1"
 frpc="/usr/bin/frpc"
 frpc_conf="/etc/frp/frpc.ini"
 frpc_log="/var/log/frp/frpc.log"
@@ -453,7 +453,7 @@ show_status(){
 update_shell(){
 	sh_new_ver=$(wget --no-check-certificate -qO- -t1 -T3 "https://raw.githubusercontent.com/Mystery0Tools/BashScripts/master/frp.sh"|grep 'sh_ver="'|awk -F "=" '{print $NF}'|sed 's/\"//g'|head -1) && sh_new_type="github"
 	[[ -z ${sh_new_ver} ]] && echo -e "${Error} 无法链接到 Github !" && exit 0
-	wget -N --no-check-certificate "https://raw.githubusercontent.com/Mystery0Tools/BashScripts/master/frp.sh" && chmod +x aria2.sh
+	wget -N --no-check-certificate "https://raw.githubusercontent.com/Mystery0Tools/BashScripts/master/frp.sh" && chmod +x frp.sh
 	echo -e "脚本已更新为最新版本[ ${sh_new_ver} ] !(注意：因为更新方式为直接覆盖当前运行的脚本，所以可能下面会提示一些报错，无视即可)" && exit 0
 }
 
